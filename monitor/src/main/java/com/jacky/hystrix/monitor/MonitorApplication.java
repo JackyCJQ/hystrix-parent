21f7dafdabcaec9b581e414145e5e663384c9f8d
jacky <348561945@qq.com>
2020-05-30 22:06:49 +0800

    code

---
+++ monitor/src/main/java/com/jacky/hystrix/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package com.jacky.hystrix.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+}
